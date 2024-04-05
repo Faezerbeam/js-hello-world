8d2ac3a09034efa464e7da46122e478d0b51f90e
Faezerbeam <140661195+Faezerbeam@users.noreply.github.com>
2024-04-05 13:24:23 -0500

    completed steps on hello.js

--- hello.js
+++ hello.js
@@ -9,7 +9,8 @@ const num2 = 5;
 
 const sum = num1 + num2;
 
-console.log("The sum of " + num1 + " and " + num2 + " is " + sum);
+console.log('The sum of ' + num1 + " and " + num2 + " is " + sum)
+console.log("This is a math word problem!",'This is a "second message!"')
 
 /*
 Next, PLAY!
