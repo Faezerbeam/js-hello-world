92ca002268feb2a97efcee2ef0898018c0dbd201
Faezerbeam <140661195+Faezerbeam@users.noreply.github.com>
2024-04-05 13:13:32 -0500

    making the numbers add together. programiz.com for the tips

--- hello.js
+++ hello.js
@@ -4,7 +4,12 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log('Hello, World!');
+const num1 = 2;
+const num2 = 5;
+
+const sum = num1 + num2;
+
+console.log("The sum of " + num1 + " and " + num2 + " is " + sum);
 
 /*
 Next, PLAY!
